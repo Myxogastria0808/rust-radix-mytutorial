de6a2683b25a922ec93b1dafa3e3ec99eb803398
Myxogastria0808 <r.rstudio.c@gmail.com>
2025-01-27 17:02:56 +0900

    feat: add radix_fmt

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
+use radix_fmt::radix;
+
 fn main() {
-    let hex_36bit = "ZZZZ";
-    println!("Hex 8-bit: {}", u32::from_str_radix(hex_36bit, 36).unwrap());
+    let hex_36bit: &str = "ZZZZ";
+    let hex_10bit: u32 = u32::from_str_radix(hex_36bit, 36).unwrap();
+    println!("10bit: {}", hex_10bit);
+    let return_hex_36bit = radix(hex_10bit, 36).to_string().to_uppercase();
+    println!("36bit: {}", return_hex_36bit);
 }
