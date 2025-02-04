f44937d91e083b8505245a07f36569f101f25612
Myxogastria0808 <r.rstudio.c@gmail.com>
2025-02-05 02:46:57 +0900

    feat: add zero filling format

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use radix_fmt::radix;
 
 fn main() {
-    let hex_36bit: &str = "ZZZZ";
+    let hex_36bit: &str = "000Z";
     let hex_10bit: u32 = u32::from_str_radix(hex_36bit, 36).unwrap();
     println!("10bit: {}", hex_10bit);
     let return_hex_36bit = radix(hex_10bit, 36).to_string().to_uppercase();
-    println!("36bit: {}", return_hex_36bit);
+    let zero_filling = format!("{:0>4}", return_hex_36bit);
+    println!("36bit: {}", zero_filling);
 }
